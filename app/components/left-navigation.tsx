daff5cec66a68787bb4714624e42270999d784c1
Ioannis Potouridis <potouridisio@gmail.com>
2022-06-24 06:57:20 +0300

    feat: add LeftNavigation component

---
+++ app/components/left-navigation.tsx
@@ -0,0 +1,11 @@
+import List from '@mui/material/List';
+import type { ListProps } from '@mui/material/List';
+
+export default function LeftNavigation(props: ListProps) {
+  return (
+    <List component="nav" {...props}>
+      {/*  */}
+      {/*  */}
+    </List>
+  );
+}
