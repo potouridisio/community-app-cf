765829dc779c663a354d49e1189f9af7280312dd
Ioannis Potouridis <potouridisio@gmail.com>
2022-06-27 10:24:10 +0300

    feat: define project breakpoints

--- app/lib/theme.ts
+++ app/lib/theme.ts
@@ -1,7 +1,23 @@
 import { createTheme } from '@mui/material/styles';
 import type { ThemeOptions } from '@mui/material/styles';
 
+declare module '@mui/material/styles' {
+  interface BreakpointOverrides {
+    '2xl': true;
+  }
+}
+
 let theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 640,
+      md: 768,
+      lg: 1024,
+      xl: 1280,
+      '2xl': 1440,
+    },
+  },
   typography: {
     fontFamily: ['"Open Sans"', 'sans-serif'].join(),
   },
