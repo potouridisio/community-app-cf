457751b436d56f82575e69bab5d227840ff02b75
Ioannis Potouridis <potouridisio@gmail.com>
2022-06-27 10:23:20 +0300

    feat: change the font family

--- app/lib/theme.ts
+++ app/lib/theme.ts
@@ -1,7 +1,11 @@
 import { createTheme } from '@mui/material/styles';
 import type { ThemeOptions } from '@mui/material/styles';
 
-let theme = createTheme();
+let theme = createTheme({
+  typography: {
+    fontFamily: ['"Open Sans"', 'sans-serif'].join(),
+  },
+});
 
 theme = createTheme(theme, {} as ThemeOptions);
 
